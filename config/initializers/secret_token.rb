c180b9cd4f23ea7d2a480d074fb9f7bcc72547b0
Becky LoveKing <rebecca.loveking@gmail.com>
2011-11-14 13:55:55 -0500

    adding rspec and capybara

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '5aeb29ea189a5eec23fb420431016b96ef1c540017d362ef722d7c1b4f4b4c599c7e92ae7aca6b6270196f6165f6f88fbe38a5d0d2b3f351d13d060b573519da'

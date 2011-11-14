09f89175ba8d5b865d6ad06b6d47a2021d7e8324
Becky LoveKing <rebecca.loveking@gmail.com>
2011-11-14 17:18:19 -0500

    Got rspec to work

--- spec/requests/widgets_spec.rb
+++ spec/requests/widgets_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Widgets" do
-  describe "GET /widgets" do
+  describe "GET /widgets", :type => :request do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get widgets_path
